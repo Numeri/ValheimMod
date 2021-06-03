a09e7b45b3267cf45b9a79daddbb5d7aa4ca8685
Numeri <Numeri@users.noreply.github.com>
2021-06-03 15:24:47 -0600

    Rename files to ValheimMod

--- ValheimMod/ValheimMod.cs
+++ ValheimMod/ValheimMod.cs
@@ -1,23 +1,20 @@
-﻿// JotunnModStub
+﻿// ValheimMod
 // a Valheim mod skeleton using Jötunn
-// 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
 
 using BepInEx;
 using UnityEngine;
 using BepInEx.Configuration;
 using Jotunn.Utils;
 
-namespace JotunnModStub
+namespace ValheimMod
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class JotunnModStub : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.jotunn.valheimmod";
+        public const string PluginName = "ValheimMod";
         public const string PluginVersion = "0.0.1";
 
         private void Awake()
